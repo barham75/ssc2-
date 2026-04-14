30220c653ef940c0d4d9bcaee2fbfccf24dea6d9
Mohammad Barham <mah.barham04777@gmail.com>
2026-04-14 18:34:30 +0300

    Redeploy with env vars

--- src/lib/posters.ts
+++ src/lib/posters.ts
@@ -10,38 +10,218 @@ export type PosterItem = {
 export const POSTERS: PosterItem[] = [
   {
     id: "p1",
-    titleAr: "Novel corrosion For Copper in 1M HCl",
-    titleEn: "Novel corrosion For Copper in 1M HCl",
-    researcherAr: "Qusai Khamaiseh",
-    researcherEn: "Qusai Khamaiseh",
-
-    // تم تحويل الرابط إلى preview
-    videoUrl:
-      "https://www.youtube.com/embed/3GJDjnhlEf8",
+    titleAr:
+      "Sustainable TiO2-Modified Spent Coffee Composite as a Light-Enhanced Antimicrobial Material",
+    titleEn:
+      "Sustainable TiO2-Modified Spent Coffee Composite as a Light-Enhanced Antimicrobial Material",
+    researcherAr: "Yousra Ali",
+    researcherEn: "Yousra Ali",
+    videoUrl: "",
   },
 
   {
     id: "p2",
-    titleAr: "Synthesis, Characterization, and Biological Activities of N-Arylacetamide with Mercapto Triazole Starting from Nalidixic acid.",
+    titleAr:
+      "Organo-Compound-Modified Cellulose Acetate (CA) Polymers for Water Treatment Applications",
     titleEn:
-      "Synthesis, Characterization, and Biological Activities of N-Arylacetamide with Mercapto Triazole Starting from Nalidixic acid.",
-    researcherAr: "Aya Jamil Jaber",
-    researcherEn: "Aya Jamil Jaber",
+      "Organo-Compound-Modified Cellulose Acetate (CA) Polymers for Water Treatment Applications",
+    researcherAr: "Shaikha A. Almajid",
+    researcherEn: "Shaikha A. Almajid",
+    videoUrl: "",
+  },
 
-    // تم تحويل الرابط إلى preview
+  {
+    id: "p3",
+    titleAr:
+      "Spectroscopic Investigation of Coumarin Derivatives for Fluorescence Sensing",
+    titleEn:
+      "Spectroscopic Investigation of Coumarin Derivatives for Fluorescence Sensing",
+    researcherAr: "Sara H. Haddad",
+    researcherEn: "Sara H. Haddad",
+    videoUrl: "",
+  },
+
+  {
+    id: "p4",
+    titleAr:
+      "Worker Pharmaceutical Innovation in Public Health Institutions",
+    titleEn:
+      "Worker Pharmaceutical Innovation in Public Health Institutions",
+    researcherAr: "Sara Mahmood",
+    researcherEn: "Sara Mahmood",
+    videoUrl: "",
+  },
+
+  {
+    id: "p5",
+    titleAr:
+      "Organic-Acid–Driven Stability in a Thiol-Engineered SiO2 Composite",
+    titleEn:
+      "Organic-Acid–Driven Stability in a Thiol-Engineered SiO2 Composite",
+    researcherAr: "Randa M. Abu Kariem",
+    researcherEn: "Randa M. Abu Kariem",
+    videoUrl: "",
+  },
+
+  {
+    id: "p6",
+    titleAr: "Novel corrosion inhibitors in 1M HCl",
+    titleEn: "Novel corrosion inhibitors in 1M HCl",
+    researcherAr: "Qusai Khamaiseh",
+    researcherEn: "Qusai Khamaiseh",
+    videoUrl: "https://www.youtube.com/embed/3GJDjnhlEf8",
+  },
+
+  {
+    id: "p7",
+    titleAr:
+      "Photophysical characteristics of coumarin derivatives",
+    titleEn:
+      "Photophysical characteristics of coumarin derivatives",
+    researcherAr: "Qais Hawarna",
+    researcherEn: "Qais Hawarna",
+    videoUrl: "",
+  },
+
+  {
+    id: "p8",
+    titleAr:
+      "Green Upcycling of Waste Green Glass into Adsorbent",
+    titleEn:
+      "Green Upcycling of Waste Green Glass into Adsorbent",
+    researcherAr: "Mais Alreem Alodeh",
+    researcherEn: "Mais Alreem Alodeh",
+    videoUrl: "",
+  },
+
+  {
+    id: "p9",
+    titleAr:
+      "Correlation of Honey Varieties by Physicochemical Content",
+    titleEn:
+      "Correlation of Honey Varieties by Physicochemical Content",
+    researcherAr: "Ileana A. Ratiu",
+    researcherEn: "Ileana A. Ratiu",
+    videoUrl: "",
+  },
+
+  {
+    id: "p10",
+    titleAr:
+      "Diatom biosilica for capillary high performance liquid chromatography",
+    titleEn:
+      "Diatom biosilica for capillary high performance liquid chromatography",
+    researcherAr: "Hussam Al Saoud",
+    researcherEn: "Hussam Al Saoud",
+    videoUrl: "",
+  },
+
+  {
+    id: "p11",
+    titleAr:
+      "Turning Transparent Glass Waste into a Sorbent",
+    titleEn:
+      "Turning Transparent Glass Waste into a Sorbent",
+    researcherAr: "Haneen Alatrash",
+    researcherEn: "Haneen Alatrash",
+    videoUrl: "",
+  },
+
+  {
+    id: "p12",
+    titleAr:
+      "Design and Evaluation of Metal-Coumarin Fluorescent Complexes",
+    titleEn:
+      "Design and Evaluation of Metal-Coumarin Fluorescent Complexes",
+    researcherAr: "Fawwaz K. Al-Husban",
+    researcherEn: "Fawwaz K. Al-Husban",
+    videoUrl: "",
+  },
+
+  {
+    id: "p13",
+    titleAr: "N.A.",
+    titleEn: "N.A.",
+    researcherAr: "Enas Aliswed",
+    researcherEn: "Enas Aliswed",
+    videoUrl: "",
+  },
+
+  {
+    id: "p14",
+    titleAr:
+      "Investigating Anticancer Activity of Deep-Sea Sponges",
+    titleEn:
+      "Investigating Anticancer Activity of Deep-Sea Sponges",
+    researcherAr: "Fatima Al-Jamal",
+    researcherEn: "Fatima Al-Jamal",
+    videoUrl: "",
+  },
+
+  {
+    id: "p15",
+    titleAr:
+      "The Role of Electron Acceptor-Donor Material",
+    titleEn:
+      "The Role of Electron Acceptor-Donor Material",
+    researcherAr: "Barajah M. Mashagba",
+    researcherEn: "Barajah M. Mashagba",
+    videoUrl: "",
+  },
+
+  {
+    id: "p16",
+    titleAr:
+      "Synthesis of N-Arylacetamide with Mercapto Triazole",
+    titleEn:
+      "Synthesis of N-Arylacetamide with Mercapto Triazole",
+    researcherAr: "Aya Jabber",
+    researcherEn: "Aya Jabber",
     videoUrl:
       "https://drive.google.com/file/d/1J4n55t3WJA3ZIJFIOSNrPHbkpKmMcRbC/preview",
   },
 
   {
-    id: "p3",
-    titleAr: "عنوان البوستر 3",
-    titleEn: "Poster Title 3",
-    researcherAr: "اسم الباحث 3",
-    researcherEn: "Researcher 3",
-
-    videoUrl: "https://www.youtube.com/embed/VIDEO_ID_3",
+    id: "p17",
+    titleAr:
+      "Green Synthesis of Zinc Oxide Nanoparticles",
+    titleEn:
+      "Green Synthesis of Zinc Oxide Nanoparticles",
+    researcherAr: "Asla A. Al-Zahrani",
+    researcherEn: "Asla A. Al-Zahrani",
+    videoUrl: "",
   },
 
-  // أكمل بنفس الطريقة حتى p30
+  {
+    id: "p18",
+    titleAr:
+      "Pyrazole and Imidazole with H. Pylori Protein",
+    titleEn:
+      "Pyrazole and Imidazole with H. Pylori Protein",
+    researcherAr: "Ammar Ibrahim",
+    researcherEn: "Ammar Ibrahim",
+    videoUrl: "",
+  },
+
+  {
+    id: "p19",
+    titleAr:
+      "Graphitic Carbon Nitride/CeO2 Nanocomposite",
+    titleEn:
+      "Graphitic Carbon Nitride/CeO2 Nanocomposite",
+    researcherAr: "Alaa S. Qargaz",
+    researcherEn: "Alaa S. Qargaz",
+    videoUrl: "",
+  },
+
+  {
+    id: "p20",
+    titleAr:
+      "Photophysical Characterization of Coumarin Analogues",
+    titleEn:
+      "Photophysical Characterization of Coumarin Analogues",
+    researcherAr: "Ahmad Mansour",
+    researcherEn: "Ahmad Mansour",
+    videoUrl: "",
+  },
 ];
